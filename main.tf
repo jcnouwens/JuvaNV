72be27284d966d1e3568c91d54f1fb6603e4a394
Jesse Nouwens <jcnouwens@gmail.com>
2024-02-13 11:17:29 +0100

    cleanup

--- main.tf
+++ main.tf
@@ -19,16 +19,11 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_databricks_workspace" "example" {
+resource "azurerm_databricks_workspace" "workspace" {
   name                = "dbw-wl-terraform-test"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "standard"
+  sku                 = "premium"
 
   tags = {
     Environment = "Test"
