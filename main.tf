5f85a9e1b435bf02208094068d75fc1f7c3cd21a
Jesse Nouwens <jcnouwens@gmail.com>
2024-02-13 10:45:05 +0100

    workspace + storage account

--- main.tf
+++ main.tf
@@ -18,3 +18,33 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-test"
   location = "westeurope"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "dbw-wl-terraform-test"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  tags = {
+    Environment = "Test"
+  }
+}
+
+resource "azurerm_storage_account" "datalake" {
+  name                     = "sawlterraformtest"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "bronze" {
+  name                  = "bronze"
+  storage_account_name  = azurerm_storage_account.datalake.name
+  container_access_type = "private"
+}
