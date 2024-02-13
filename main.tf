40eda6418e53f07470cb29ad02ec1eb59880b95b
Ravi Gauri <Raga20@juva.nl>
2024-02-13 11:51:16 +0100

    Push

--- main.tf
+++ main.tf
@@ -31,15 +31,16 @@ resource "azurerm_databricks_workspace" "workspace" {
 }
 
 resource "azurerm_storage_account" "datalake" {
-  name                     = "sawlterraformtest"
+  name                     = "sawlterraformtest2"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  is_hns_enabled           = "true"
 }
 
-resource "azurerm_storage_container" "bronze" {
-  name                  = "bronze"
-  storage_account_name  = azurerm_storage_account.datalake.name
-  container_access_type = "private"
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "bronze" {
+  name               = "bronze"
+  storage_account_id = azurerm_storage_account.datalake.id
 }
